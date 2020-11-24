e4f1851b503befacdb56b35e49b3efdec1ef8da6
normen <normen667@gmail.com>
2020-11-24 01:38:29 +0100

    cleanup settings

--- config/settings.go
+++ config/settings.go
@@ -6,7 +6,6 @@ import (
 	"os/user"
 
 	"github.com/adrg/xdg"
-	"github.com/gdamore/tcell/v2"
 	"gitlab.com/tslocum/cbind"
 	"gopkg.in/ini.v1"
 )
@@ -63,57 +62,62 @@ type Colors struct {
 	Negative        string
 }
 
+var Config = IniFile{
+	&General{
+		DownloadPath: GetHomeDir() + "Downloads",
+		PreviewPath:  GetHomeDir() + "Downloads",
+		CmdPrefix:    "/",
+	},
+	&Keymap{
+		SwitchPanels:    "Tab",
+		FocusMessages:   "Ctrl+w",
+		FocusInput:      "Ctrl+Space",
+		FocusContacts:   "Ctrl+e",
+		CommandBacklog:  "Ctrl+b",
+		CommandConnect:  "Ctrl+r",
+		CommandQuit:     "Ctrl+q",
+		CommandHelp:     "Ctrl+?",
+		MessageDownload: "d",
+		MessageInfo:     "i",
+		MessageOpen:     "o",
+		MessageRevoke:   "r",
+		MessageShow:     "s",
+	},
+	&Ui{
+		ContactSidebarWidth: 30,
+	},
+	&Colors{
+		Background:      "black",
+		Text:            "white",
+		ListHeader:      "yellow",
+		ListContact:     "green",
+		ListGroup:       "blue",
+		ChatContact:     "green",
+		ChatMe:          "blue",
+		Borders:         "white",
+		InputBackground: "blue",
+		InputText:       "white",
+		Positive:        "green",
+		Negative:        "red",
+	},
+}
+
 func InitConfig() {
-	defaultCfg := &IniFile{
-		&General{
-			DownloadPath: GetHomeDir() + "Downloads",
-			PreviewPath:  GetHomeDir() + "Downloads",
-			CmdPrefix:    "/",
-		},
-		&Keymap{
-			SwitchPanels:    "Tab",
-			FocusMessages:   "Ctrl+w",
-			FocusInput:      "Ctrl+Space",
-			FocusContacts:   "Ctrl+e",
-			CommandBacklog:  "Ctrl+b",
-			CommandConnect:  "Ctrl+r",
-			CommandQuit:     "Ctrl+q",
-			CommandHelp:     "Ctrl+?",
-			MessageDownload: "d",
-			MessageInfo:     "i",
-			MessageOpen:     "o",
-			MessageRevoke:   "r",
-			MessageShow:     "s",
-		},
-		&Ui{
-			ContactSidebarWidth: 30,
-		},
-		&Colors{
-			Background:      "black",
-			Text:            "white",
-			ListHeader:      "yellow",
-			ListContact:     "green",
-			ListGroup:       "blue",
-			ChatContact:     "green",
-			ChatMe:          "blue",
-			Borders:         "white",
-			InputBackground: "blue",
-			InputText:       "white",
-			Positive:        "green",
-			Negative:        "red",
-		},
-	}
 	var err error
 	if configFilePath, err = xdg.ConfigFile("whatscli/whatscli.config"); err == nil {
 		// add any new values
-		if err = ini.MapToWithMapper(defaultCfg, ini.TitleUnderscore, configFilePath); err == nil {
-			cfg = ini.Empty()
-			if err = ini.ReflectFromWithMapper(cfg, defaultCfg, ini.TitleUnderscore); err == nil {
-				err = cfg.SaveTo(configFilePath)
+		var cfg *ini.File
+		if cfg, err = ini.Load(configFilePath); err == nil {
+			cfg.NameMapper = ini.TitleUnderscore
+			if err = cfg.MapTo(&Config); err == nil {
+				if err = ini.ReflectFromWithMapper(cfg, &Config, ini.TitleUnderscore); err == nil {
+					err = cfg.SaveTo(configFilePath)
+				}
 			}
 		} else {
 			cfg = ini.Empty()
-			if err = ini.ReflectFromWithMapper(cfg, defaultCfg, ini.TitleUnderscore); err == nil {
+			cfg.NameMapper = ini.TitleUnderscore
+			if err = ini.ReflectFromWithMapper(cfg, &Config, ini.TitleUnderscore); err == nil {
 				err = cfg.SaveTo(configFilePath)
 			}
 		}
@@ -139,52 +143,6 @@ func GetContactsFilePath() string {
 		return sessionFilePath
 	}
 	return GetHomeDir() + ".whatscli.contacts"
-}
-
-func GetKey(name string) string {
-	if sec, err := cfg.GetSection("keymap"); err == nil {
-		if key, err := sec.GetKey(name); err == nil {
-			return key.String()
-		}
-	}
-	return ""
-}
-
-func GetColorName(key string) string {
-	if sec, err := cfg.GetSection("colors"); err == nil {
-		if key, err := sec.GetKey(key); err == nil {
-			return key.String()
-		}
-	}
-	return "white"
-}
-
-func GetColor(key string) tcell.Color {
-	name := GetColorName(key)
-	if color, ok := tcell.ColorNames[name]; ok {
-		return color
-	}
-	return tcell.ColorWhite
-}
-
-func GetSetting(name string) string {
-	if sec, err := cfg.GetSection("general"); err == nil {
-		if key, err := sec.GetKey(name); err == nil {
-			return key.String()
-		}
-	}
-	return ""
-}
-
-func GetIntSetting(section string, name string) int {
-	if sec, err := cfg.GetSection(section); err == nil {
-		if key, err := sec.GetKey(name); err == nil {
-			if val, err := key.Int(); err == nil {
-				return val
-			}
-		}
-	}
-	return 0
 }
 
 // gets the OS home dir with a path separator at the end
