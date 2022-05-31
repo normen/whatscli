f0bcf07f97a845c97685a1ca8d3ec7e6a60392e1
Anna “CyberTailor” <cyber@sysrq.in>
2022-05-31 05:02:39 +0500

    release.sh: generate releae tarball
    Vendor all dependenices to make packaging easier.

--- release.sh
+++ release.sh
@@ -7,10 +7,15 @@ else
   VERSION=$1
 fi
 echo Releasing $VERSION
+TARF=whatscli-${VERSION#v}.tar.gz
 WINF=whatscli-$VERSION-windows.zip
 LINUXF=whatscli-$VERSION-linux.zip
 MACF=whatscli-$VERSION-macos.zip
 RASPIF=whatscli-$VERSION-raspberrypi.zip
+
+# create release tarball
+go mod vendor
+tar czf $TARF --transform "s,^,whatscli-${VERSION#v}/," *
 
 # build zip files with binaries
 GOOS=darwin go build -o whatscli
@@ -31,7 +36,7 @@ git pull
 LASTTAG=$(git describe --tags --abbrev=0)
 git log $LASTTAG..HEAD --no-decorate --pretty=format:"- %s" --abbrev-commit > changes.txt
 vim changes.txt
-gh release create $VERSION $LINUXF $MACF $WINF $RASPIF -F changes.txt -t $VERSION
+gh release create $VERSION $TARF $LINUXF $MACF $WINF $RASPIF -F changes.txt -t $VERSION
 rm changes.txt
 rm *.zip
 
