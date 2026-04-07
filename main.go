9bcf7fff1df8ca7347ea345661bfbecbb3b5c7b9
Normen <normen667@gmail.com>
2026-04-07 23:02:15 +0200

    v1.1.3 - backlog to the future

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/zyedidia/clipboard"
 )
 
-var VERSION string = "v1.1.2"
+var VERSION string = "v1.1.3"
 
 var sndTxt string = ""
 var currentReceiver messages.Chat = messages.Chat{}
