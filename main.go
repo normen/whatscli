85aab050a7ad3d185a6de749e28d723fb6b819de
Normen <normen667@gmail.com>
2026-04-07 22:11:26 +0200

    bump version

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/zyedidia/clipboard"
 )
 
-var VERSION string = "v1.1.0"
+var VERSION string = "v1.1.1"
 
 var sndTxt string = ""
 var currentReceiver messages.Chat = messages.Chat{}
